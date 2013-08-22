9342c5d8cee80908c1887fdb5c8d9c0620ddbc15
Richard Harrah <topplethenunnery@gmail.com>
2013-08-22 09:04:33 -0400

    updating and fixing some of RewardManager

--- src/main/java/net/nunnerycode/bukkit/mobbountyreloaded/managers/RewardManager.java
+++ src/main/java/net/nunnerycode/bukkit/mobbountyreloaded/managers/RewardManager.java
@@ -1,53 +1,58 @@
 package net.nunnerycode.bukkit.mobbountyreloaded.managers;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import net.nunnerycode.bukkit.mobbountyreloaded.MobBountyReloadedPlugin;
+import net.nunnerycode.bukkit.mobbountyreloaded.utils.RandomRangeUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 
 public class RewardManager {
 
 	private final MobBountyReloadedPlugin plugin;
+	private final Map<String, Double> rewardValue;
 
 	public RewardManager(MobBountyReloadedPlugin plugin) {
 		this.plugin = plugin;
+		rewardValue = new HashMap<String, Double>();
 	}
 
-	public double getValue(String worldName, String type) {
-		String s;
-		if (getPlugin().getSettings().getRewards().containsKey(worldName + "." + type)) {
-			s = getPlugin().getSettings().getRewards().get(worldName + "." + type);
-		} else if (getPlugin().getSettings().getRewards().containsKey("Default." + type)) {
-			s = getPlugin().getSettings().getRewards().get("Default." + type);
-		} else {
-			s = "0.0";
+	public void updateRewardValue() {
+		Map<String, String> rewards = getPlugin().getSettings().getRewards();
+		for (World w : Bukkit.getWorlds()) {
+			String worldName = w.getName();
+			String typeName;
+			String s;
+			for (EntityType entityType : EntityType.values()) {
+				typeName = entityType.name();
+				if (rewards.containsKey(worldName + "." + typeName)) {
+					s = rewards.get(worldName + "." + typeName);
+				} else if (rewards.containsKey("Default." + typeName)) {
+					s = rewards.get("Default." + typeName);
+				} else {
+					s = "0.0";
+				}
+				if (s.contains(":")) {
+					rewardValue.put(worldName + "." + typeName, handleRange(s, ":", 0.0D));
+				}
+				rewardValue.put(worldName + "." + typeName, getDouble(s, 0.0D));
+			}
 		}
-		if (s.contains(":")) {
-			return handleRange(s, ":", 0);
-		}
-		return getInt(s, 0);
 	}
 
 	public MobBountyReloadedPlugin getPlugin() {
 		return plugin;
 	}
 
-	private int handleRange(String string, String splitter, int fallback) {
+	private double handleRange(String string, String splitter, double fallback) {
 		Random rand = new Random();
-		int result;
-		int range;
-		int loc;
 		String[] splitString = string.split(splitter);
 		if (splitString.length > 1) {
-			if (getDouble(splitString[0], 0) > getDouble(splitString[1], 0)) {
-				range = (int) ((getDouble(splitString[0], 0) * 100) - (getDouble(
-						splitString[1], 0) * 100));
-				loc = (int) (getDouble(splitString[1], 0) * 100);
-			} else {
-				range = (int) ((getDouble(splitString[1], 0) * 100) - (getDouble(
-						splitString[0], 0) * 100));
-				loc = (int) (getDouble(splitString[0], 0) * 100);
-			}
-			result = (loc + rand.nextInt(range + 1)) / 100;
-			return result;
+			return RandomRangeUtils.randomRangeDoubleInclusive(getDouble(splitString[0], 0.0D),
+					getDouble(splitString[1], 0.0D));
 		}
 		return fallback;
 	}
@@ -62,6 +67,13 @@ public class RewardManager {
 		return d;
 	}
 
+	public double getValue(String worldName, String type) {
+		if (getRewardValue().containsKey(worldName + "." + type)) {
+			return getRewardValue().get(worldName + "." + type);
+		}
+		return 0.0D;
+	}
+
 	private int getInt(String string, int fallBack) {
 		int i;
 		try {
@@ -72,19 +84,7 @@ public class RewardManager {
 		return i;
 	}
 
-	private int handleRange(int i1, int i2) {
-		Random rand = new Random();
-		int result;
-		int range;
-		int loc;
-		if ((double) i1 > (double) i2) {
-			range = (int) (((double) i1 * 100) - ((double) i2 * 100));
-			loc = (int) ((double) i2 * 100);
-		} else {
-			range = (int) (((double) i2 * 100) - ((double) i1 * 100));
-			loc = (int) ((double) i1 * 100);
-		}
-		result = (loc + rand.nextInt(range + 1)) / 100;
-		return result;
+	public Map<String, Double> getRewardValue() {
+		return Collections.unmodifiableMap(rewardValue);
 	}
 }
