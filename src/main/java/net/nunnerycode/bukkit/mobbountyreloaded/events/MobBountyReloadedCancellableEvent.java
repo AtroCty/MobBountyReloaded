1a17208062bf347c887f709cd2409a7d5c80120e
Richard Harrah <topplethenunnery@gmail.com>
2013-08-22 09:42:06 -0400

    adding event parents

---
+++ src/main/java/net/nunnerycode/bukkit/mobbountyreloaded/events/MobBountyReloadedCancellableEvent.java
@@ -0,0 +1,18 @@
+package net.nunnerycode.bukkit.mobbountyreloaded.events;
+
+import org.bukkit.event.Cancellable;
+
+public class MobBountyReloadedCancellableEvent extends MobBountyReloadedEvent implements Cancellable {
+
+	private boolean cancelled;
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+}
---
+++ src/main/java/net/nunnerycode/bukkit/mobbountyreloaded/events/MobBountyReloadedEvent.java
@@ -0,0 +1,19 @@
+package net.nunnerycode.bukkit.mobbountyreloaded.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class MobBountyReloadedEvent extends Event {
+
+	private static final HandlerList HANDLER_LIST = new HandlerList();
+
+	public static HandlerList getHandlerList() {
+		return HANDLER_LIST;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return HANDLER_LIST;
+	}
+
+}
